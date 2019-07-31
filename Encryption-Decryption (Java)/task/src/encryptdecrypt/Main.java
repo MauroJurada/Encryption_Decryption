8bc90789b8c1f41c745f9cf084cb85174dd25ab5
Mauro Jurada <37582356+MauroJurada@users.noreply.github.com>
2019-07-31 22:50:53 +0200

    stage #3

--- Encryption-Decryption (Java)/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption (Java)/task/src/encryptdecrypt/Main.java
@@ -1,29 +1,42 @@
 package encryptdecrypt;
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        String line = scanner.nextLine().toLowerCase(Locale.ENGLISH);
+
+        String mode = scanner.nextLine();
+        String line = scanner.nextLine();
         int key = scanner.nextInt();
-        //String line = "welcome to hyperskill".toLowerCase(Locale.ENGLISH);
+
+        //String line = "Welcome to hyperskill!";
         //int key = 5;
 
+        if (mode.equals("enc")){
+            System.out.println(encrypt(line, key));
+        } else {
+            System.out.println(decrypt(line, key));
+        }
+    }
+    
+    private static String encrypt(String line, int key){
         String cyph = "";
         char x;
 
         for (int i = 0; i < line.length(); i++){
-            x = line.charAt(i);
-            if (x > 96 && x < 123){
-                if (key+x > 122) x -= 26;
-                cyph = cyph.concat( Character.toString((char)(x+key)));
-            } else {
-                cyph = cyph.concat(Character.toString(x));
-            }
+            cyph = cyph.concat( Character.toString((char)(line.charAt(i)+key)%127));
         }
+        return cyph;
+    }
 
-        System.out.println(cyph);
+    private static String decrypt(String line, int key){
+        String cyph = "";
+        char x;
+
+        for (int i = 0; i < line.length(); i++){
+            cyph = cyph.concat( Character.toString((char)(line.charAt(i)-key)%127));
+        }
+        return cyph;
     }
 }
