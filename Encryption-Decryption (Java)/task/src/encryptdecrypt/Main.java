1c34a2948970aa69bf146deb05bf5a7bc3f880f4
Mauro Jurada <37582356+MauroJurada@users.noreply.github.com>
2019-07-31 22:05:36 +0200

    stage #2

--- Encryption-Decryption (Java)/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption (Java)/task/src/encryptdecrypt/Main.java
@@ -1,7 +1,29 @@
 package encryptdecrypt;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("dv ulfmw z givzhfiv!");
+
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine().toLowerCase(Locale.ENGLISH);
+        int key = scanner.nextInt();
+        //String line = "welcome to hyperskill".toLowerCase(Locale.ENGLISH);
+        //int key = 5;
+
+        String cyph = "";
+        char x;
+
+        for (int i = 0; i < line.length(); i++){
+            x = line.charAt(i);
+            if (x > 96 && x < 123){
+                if (key+x > 122) x -= 26;
+                cyph = cyph.concat( Character.toString((char)(x+key)));
+            } else {
+                cyph = cyph.concat(Character.toString(x));
+            }
+        }
+
+        System.out.println(cyph);
     }
 }
