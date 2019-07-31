2be471defc6af209f6e14e1cf4bd8b17840f2304
Mauro Jurada <37582356+MauroJurada@users.noreply.github.com>
2019-08-01 01:29:11 +0200

    stage #4

--- Encryption-Decryption (Java)/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption (Java)/task/src/encryptdecrypt/Main.java
@@ -1,4 +1,5 @@
 package encryptdecrypt;
+
 import java.util.Scanner;
 
 public class Main {
@@ -6,34 +7,54 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        String mode = scanner.nextLine();
-        String line = scanner.nextLine();
-        int key = scanner.nextInt();
+        String mode = "enc";
+        String line = null;
+        int key = 0;
 
-        //String line = "Welcome to hyperskill!";
-        //int key = 5;
+        for (int i = 0; i < args.length; i += 2) {
+            switch (args[i]){
+                case "-mode":
+                    mode = args[i+1];
+                    break;
+                case "-data":
+                    line = args[i+1];
+                    break;
+                case "-key":
+                    key = Integer.parseInt(args[i+1]);
+                    break;
+            }
+        }
 
-        if (mode.equals("enc")){
+/*          If the -data argument is not specified, the program must wait
+            for it and the -key argument from the standard input
+
+        if (line.isEmpty()){
+            line = scanner.nextLine();
+            key = scanner.nextInt();
+        }
+*/
+
+        if (mode.equals("enc")) {
             System.out.println(encrypt(line, key));
         } else {
             System.out.println(decrypt(line, key));
         }
     }
-    
-    private static String encrypt(String line, int key){
+
+    private static String encrypt(String line, int key) {
         String cyph = "";
 
-        for (int i = 0; i < line.length(); i++){
-            cyph = cyph.concat( Character.toString((char)(line.charAt(i)+key)%127));
+        for (int i = 0; i < line.length(); i++) {
+            cyph = cyph.concat(Character.toString((char) (line.charAt(i) + key) % 127));
         }
         return cyph;
     }
 
-    private static String decrypt(String line, int key){
+    private static String decrypt(String line, int key) {
         String cyph = "";
 
-        for (int i = 0; i < line.length(); i++){
-            cyph = cyph.concat( Character.toString((char)(line.charAt(i)-key)%127));
+        for (int i = 0; i < line.length(); i++) {
+            cyph = cyph.concat(Character.toString((char) (line.charAt(i) - key) % 127));
         }
         return cyph;
     }
